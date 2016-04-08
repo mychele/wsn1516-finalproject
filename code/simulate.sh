26b8d9c7656d1917109a0635443c0cdb3efe8fba
oliva18 <oliva18@gmail.com>
2016-04-08 08:59:51 +0200

    bash output in a .log file

--- code/simulate.sh
+++ code/simulate.sh
@@ -3,6 +3,7 @@
 
 
 rm *.txt
+rm *.log
 echo -e "sent\tinfile\ttime\tgoodput\tthroughput\tPER_estimate\tPER\tK_TB_SIZE\tN_TB_SIZE" >> "logtx".txt
 echo -e "dectime\trxtime\ttotrxpck\tdroppck\trxpck\tuselesspck\tPER_estimate\tPER\tK_TB_SIZE\tN_TB_SIZE" >> "logrx".txt
 
@@ -11,8 +12,8 @@ do
 	for INCREMENT in 1000 1250 1500 1750 2000;
 	do
 		N_TB_SIZE_VAL=$((K_TB_SIZE_VAL+INCREMENT))
-		echo "K_TB_SIZE=$K_TB_SIZE_VAL"
-		echo "N_TB_SIZE=$N_TB_SIZE_VAL"
+		echo "K_TB_SIZE=$K_TB_SIZE_VAL" >> "report".log
+		echo "N_TB_SIZE=$N_TB_SIZE_VAL" >> "report".log
 		make clear
 		sed "s/K_TB_SIZE_VALUE/$K_TB_SIZE_VAL/g" NCpacket.h.template > NCpacket.h.tmp
 		sed "s/N_TB_SIZE_VALUE/$N_TB_SIZE_VAL/g" NCpacket.h.tmp > NCpacket.h
@@ -21,22 +22,22 @@ do
 		rm NCpacket.h.tmp
 
 
-		echo $1
+		echo $1 >> "report".log
 
 		for PER in 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8
 		do
 
-			echo "PER=$PER"
+			echo "PER=$PER" >> "report".log
 			i=1
 			while [ $i -le 10 ]
 			do
-				echo $i
-				timeout 120 ./receiver audio.wav $PER >> "logrx".txt & # $1 is filename, $2 is PER, in background
+				echo $i >> "report".log
+				timeout 240 ./receiver audio.wav $PER >> "logrx".txt & # $1 is filename, $2 is PER, in background
 				sleep 1
-				timeout 120 ./sender localhost localhost $1 $PER >> "logtx".txt # $1 is filename
+				timeout 240 ./sender localhost localhost $1 $PER >> "logtx".txt # $1 is filename
 				#simulate $1 $2
 				status=$?
-				echo $status
+				echo "exit status of timeout: "$status >> "report".log
 				if [ $status -ne 124 ]
 					then
 						i=$(( i+1 ))
