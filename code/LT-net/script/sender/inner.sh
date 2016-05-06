87f2fb31753e209d9d1eef33002febf571039eb3
mychele <michele.polese@gmail.com>
2016-05-06 09:42:10 +0200

    Data for sender in Padova-Losanna transmissions

--- code/LT-net/script/sender/inner.sh
+++ code/LT-net/script/sender/inner.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+cd /home/michele/repos/wsn1516-finalproject/code/LT-net/script/sender
+
 arr_K=(2500 3500 4500)
 arr_increment=(1000 1500)
 source K_index_file
 source increment_index_file
 source trial_index_file
-if [ $trial_index -lt 2 ]
+if [ $trial_index -lt 10 ]
 	then
 	trial_index=$((trial_index+1))
 	echo trial_index=$trial_index > trial_index_file
@@ -23,7 +26,7 @@ else
 			then
 			echo K_index=$K_index > K_index_file
 			else
-			K_index=-1
+			K_index=0
 			echo "end of operations" >> "report".log
 			echo "end of operations" >>  "logtx".txt
 		fi
@@ -36,8 +39,8 @@ echo $trial_index >> "report".log
 ../../compile.sh $K $increment
 sleep 30
 timeout 240 ../../sender 192.168.1.100 128.178.70.97 ../../$1 0 >> "logtx".txt
-echo "exit status of timeout: "$status >> "report".log
-if [ $status -ne 124 ]
+echo "exit status of timeout: "$? >> "report".log
+if [ $? -eq 124 ]
 then
 	trial_index=$((trial_index-1))
 	echo trial_index=$trial_index > trial_index_file
--- code/LT-net/script/sender/setup.sh
+++ code/LT-net/script/sender/setup.sh
@@ -6,4 +6,4 @@ echo K_index=0 > K_index_file
 echo trial_index=0 > trial_index_file
 echo -e "sent\tinfile\ttime\tgoodput\tthroughput\tPER_estimate\tPER\tK_TB_SIZE\tN_TB_SIZE" >> "logtx".txt
 
-(crontab -l 2>/dev/null; echo "*/5 * * * * /home/stefano/inner.sh $1") | crontab -
+#(crontab -l 2>&1/home/michele/crontaberror.txt; echo "*/1 * * * * /home/michele/repos/wsn1516-finalproject/code/LT-net/script/sender/inner.sh") | crontab -
